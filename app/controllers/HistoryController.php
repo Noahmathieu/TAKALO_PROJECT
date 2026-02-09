cc45061e0d8eac69b447e24f5c0da3bc5be53bb3
Noahmathieu <noahtown011@gmail.com>
2026-02-09 17:00:01 +0300

    History change

---
+++ app/controllers/HistoryController.php
@@ -0,0 +1,12 @@
+<?php
+class HistoryController {
+
+  public static function showHistory() {
+    $id_objet = Flight::request()->data->id_objet;
+    $pdo = Flight::db();
+    $historyModel = new \App\Models\HistoryModel($pdo);
+    $history = $historyModel->history($id_objet);
+    Flight::render('history/history', ['history' => $history]);
+  }
+
+}
