c865d090b9d072e368a653d79ed1e7a9d034a458
Mitiafyh <mitiafiorenantsoa@gmail.com>
2026-02-13 11:59:01 +0300

    Nouveau FINAL

--- app/views/history/history.php
+++ app/views/history/history.php
@@ -15,8 +15,8 @@ include __DIR__ . '/../partials/header.php';
                 <table class="table table-striped table-bordered align-middle">
                     <thead class="table-light">
                         <tr>
-                            <th>Propriétaire 1</th>
-                            <th>Propriétaire 2</th>
+                            <th>Ancien Propriétaire 1</th>
+                            <th>Propriétaire Actuel</th>
                             <th>Date</th>
                         </tr>
                     </thead>
